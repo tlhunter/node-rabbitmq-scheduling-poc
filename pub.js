3c25210ae4770d28b7a3429d9fc9c21aad801885
Thomas Hunter II <me@thomashunter.name>
2014-12-01 23:51:49 -0800

    pub using callbacks

--- pub.js
+++ pub.js
@@ -1,34 +1,27 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
+var amqp = require('amqplib/callback_api');
 
 var EXCHANGE_NAME = 'example-pubsub';
 var MESSAGE_COUNT = 10000;
 var TICK = Math.floor(MESSAGE_COUNT / 100);
 
-amqp.connect('amqp://localhost').then(function(conn) {
+amqp.connect('amqp://localhost', function(err, conn) {
 	process.once('SIGINT', function() { conn.close(); });
 
-	return conn.createChannel().then(function(channel) {
-		var chok = channel.assertExchange(EXCHANGE_NAME, 'direct', {
-			durable: true
-		});
+	conn.createChannel(function(err, channel) {
+		channel.assertExchange(EXCHANGE_NAME, 'direct', { durable: true });
 
-		chok.then(function() {
-			// TODO: This blocks like you wouldn't believe...
-			for (var i = 1; i < MESSAGE_COUNT; i++) {
-				sender(i);
+		// TODO: This blocks like you wouldn't believe...
+		for (var i = 0; i < MESSAGE_COUNT; i++) {
+			sender(i);
 
-				if (i % TICK === 0) {
-					console.log(i / TICK + '%');
-				}
+			if (i % TICK === 0) {
+				console.log(i / TICK + '%');
 			}
+		}
 
-			return sender('FIN');
-
-		});
-
-		return chok;
+		sender('FIN');
 
 		function sender(i) {
 			channel.publish(EXCHANGE_NAME, '', new Buffer(JSON.stringify({
@@ -36,4 +29,4 @@ amqp.connect('amqp://localhost').then(function(conn) {
 			})));
 		}
 	});
-}).then(null, console.warn);
+});
