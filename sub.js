49dc6f5b55befbec61b82629ef49073cff23edfb
Thomas Hunter II <me@thomashunter.name>
2014-12-02 00:11:08 -0800

    sub uses callbacks

--- sub.js
+++ sub.js
@@ -1,47 +1,33 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
+var amqp = require('amqplib/callback_api');
 var os = require('os');
 var sleep = require('sleep');
 
 var QUEUE_NAME = process.pid + '@' + os.hostname(); // Unique queue name per process
 var EXCHANGE_NAME = 'example-pubsub';
 
-amqp.connect('amqp://localhost').then(function(conn) {
+amqp.connect('amqp://localhost', function(err, conn) {
 	process.once('SIGINT', function() { conn.close(); });
 
-	return conn.createChannel().then(function(channel) {
-		var ok = channel.assertExchange(EXCHANGE_NAME, 'direct', {
-			durable: true
-		});
+	conn.createChannel(function(err, channel) {
+		channel.assertExchange(EXCHANGE_NAME, 'direct', { durable: true });
 
-		ok = ok.then(function() {
-			return channel.assertQueue(QUEUE_NAME, {
-				exclusive: true
-			});
-		});
+		channel.assertQueue(QUEUE_NAME, { exclusive: true }, function(err, qok) {
+			channel.bindQueue(qok.queue, EXCHANGE_NAME, '');
 
-		ok = ok.then(function(qok) {
-			return channel.bindQueue(qok.queue, EXCHANGE_NAME, '').then(function() {
-				return qok.queue;
-			});
-		});
-
-		ok = ok.then(function(queue) {
-			return channel.consume(queue, function(msg) {
+			channel.consume(qok.queue, function(msg) {
 				console.log(msg.content.toString());
 				sleepMS(10); // block
-				return channel.ack(msg);
+				channel.ack(msg);
 			}, {
 				noAck: false
+			}, function() {
+				console.log('Waiting for messages delivered to queue: ' + QUEUE_NAME);
 			});
 		});
-
-		return ok.then(function() {
-			console.log('Waiting for messages delivered to queue: ' + QUEUE_NAME);
-		});
 	});
-}).then(null, console.warn);
+});
 
 function sleepMS(ms) {
 	sleep.usleep(ms * 1000); // usleep is in microseconds
