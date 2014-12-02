1071458385beee8cacd2458646c309a22e850ca5
Thomas Hunter II <me@thomashunter.name>
2014-12-01 23:43:00 -0800

    simple example

---
+++ simple-pub-sub.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+// Simple Example, taken from https://github.com/squaremo/amqp.node
+
+var QUEUE_NAME = 'tasks';
+
+function bail(err) {
+  console.error(err);
+  process.exit(1);
+}
+
+// Publisher
+function publisher(conn) {
+  conn.createChannel(function(err, ch) {
+    if (err !== null) {
+		return bail(err);
+	}
+
+    ch.assertQueue(QUEUE_NAME);
+
+    ch.sendToQueue(QUEUE_NAME, new Buffer('something to do'));
+  });
+}
+
+// Consumer
+function consumer(conn) {
+  conn.createChannel(function(err, ch) {
+    if (err !== null) {
+		return bail(err);
+	}
+
+    ch.assertQueue(QUEUE_NAME);
+
+    ch.consume(QUEUE_NAME, function(msg) {
+      if (msg !== null) {
+        console.log(msg.content.toString());
+        ch.ack(msg);
+      }
+    });
+  });
+}
+
+require('amqplib/callback_api').connect('amqp://localhost', function(err, conn) {
+  if (err !== null) {
+    return bail(err);
+  }
+
+  consumer(conn);
+  publisher(conn);        
+});
